0471107c319c77e096ad3843d0a0f4e6765b17ce
l33th <l33th@protonmail.ch>
2021-11-30 09:01:29 +0200

    header styles

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const CoinPage = () => {
-	return <div>coin page</div>;
+	return (
+		<div>
+			<h1>Coin Page</h1>
+		</div>
+	);
 };
 
 export default CoinPage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Container, makeStyles, MenuItem, Select } from '@material-ui/core';
+import {
+	Container,
+	createTheme,
+	makeStyles,
+	MenuItem,
+	Select,
+	ThemeProvider,
+} from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -9,10 +17,10 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
 	title: {
-		flexGrow: 1,
-		text: 1,
+		// flexGrow: 1,
+		flex: 1,
 		color: 'cyan',
-		fontFamily: 'Montserrat',
+		fontFamily: 'MontserratAlternates',
 		fontWeight: 'bold',
 		cursor: 'pointer',
 	},
@@ -23,35 +31,54 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = () => {
 	const classes = useStyles();
+	const navigate = useNavigate();
+	const darkTheme = createTheme({
+		palette: {
+			primary: {
+				main: '#fff',
+			},
+			type: 'dark',
+		},
+	});
 	return (
-		<div className={classes.root}>
-			<AppBar color='transparent' position='static'>
-				<Container>
-					<Toolbar>
-						<IconButton
-							edge='start'
-							className={classes.menuButton}
-							color='inherit'
-							aria-label='menu'
-						>
-							<MenuIcon />
-						</IconButton>
-						<Typography variant='h6' className={classes.title}>
-							Cryptracer
-						</Typography>
-						<Select
-							variant='outlined'
-							style={{ width: 100, height: 40, marginLeft: 15 }}
-						>
-							<MenuItem value={'USD'}>USD</MenuItem>
-							<MenuItem value={'ZAR'}>ZAR</MenuItem>
-						</Select>
-						<Button color='inherit'>Login</Button>
-						<Button color='inherit'>Signup</Button>
-					</Toolbar>
-				</Container>
-			</AppBar>
-		</div>
+		<ThemeProvider theme={darkTheme}>
+			<div className={classes.root}>
+				<AppBar color='transparent' position='static'>
+					<Container>
+						<Toolbar>
+							<IconButton
+								edge='start'
+								className={classes.menuButton}
+								color='inherit'
+								aria-label='menu'
+							>
+								<MenuIcon />
+							</IconButton>
+							<Typography
+								onClick={() => navigate('/')}
+								variant='h6'
+								className={classes.title}
+							>
+								Cryptracer
+							</Typography>
+							<Select
+								variant='outlined'
+								style={{
+									width: 100,
+									height: 40,
+									marginLeft: 15,
+								}}
+							>
+								<MenuItem value={'USD'}>USD</MenuItem>
+								<MenuItem value={'ZAR'}>ZAR</MenuItem>
+							</Select>
+							<Button color='inherit'>Login</Button>
+							<Button color='inherit'>Signup</Button>
+						</Toolbar>
+					</Container>
+				</AppBar>
+			</div>
+		</ThemeProvider>
 	);
 };
 
