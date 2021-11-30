20bc2232f24de7d915a0e9f25a5ea805bed7ece9
l33th <l33th@protonmail.ch>
2021-11-30 08:33:17 +0200

    cryptracer title style and selector

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, makeStyles } from '@material-ui/core';
+import { Container, makeStyles, MenuItem, Select } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -8,14 +8,16 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
-	root: {
+	title: {
 		flexGrow: 1,
+		text: 1,
+		color: 'cyan',
+		fontFamily: 'Montserrat',
+		fontWeight: 'bold',
+		cursor: 'pointer',
 	},
 	menuButton: {
 		marginRight: theme.spacing(2),
-	},
-	title: {
-		flexGrow: 1,
 	},
 }));
 
@@ -37,6 +39,13 @@ const Header = () => {
 						<Typography variant='h6' className={classes.title}>
 							Cryptracer
 						</Typography>
+						<Select
+							variant='outlined'
+							style={{ width: 100, height: 40, marginLeft: 15 }}
+						>
+							<MenuItem value={'USD'}>USD</MenuItem>
+							<MenuItem value={'ZAR'}>ZAR</MenuItem>
+						</Select>
 						<Button color='inherit'>Login</Button>
 						<Button color='inherit'>Signup</Button>
 					</Toolbar>
