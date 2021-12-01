9d36794f6c85c8a8f3a684b13cbc8deb5a068b59
l33th <l33th@protonmail.ch>
2021-12-01 11:42:44 +0200

    cryptocurrency prices by market cap

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { CoinList } from '../config/api';
+import { CryptoState } from '../CryptoContext';
+import {
+	Container,
+	createTheme,
+	MuiThemeProvider,
+	Typography,
+} from '@material-ui/core';
 
 export function numberWithCommas(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 const CoinsTable = () => {
-	return <div>CoinsTable</div>;
+	const [coins, setCoins] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	const { currency, symbol } = CryptoState();
+
+	const fetchCoins = async () => {
+		setLoading(true);
+		const { data } = await axios.get(CoinList(currency));
+		console.log(data);
+
+		setCoins(data);
+		setLoading(false);
+	};
+
+	console.log(coins);
+
+	useEffect(() => {
+		fetchCoins();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currency]);
+
+	const darkTheme = createTheme({
+		palette: {
+			primary: {
+				main: '#fff',
+			},
+			type: 'dark',
+		},
+	});
+
+	return (
+		<>
+			<MuiThemeProvider theme={darkTheme}>
+				<Container style={{ textAlign: 'center' }}>
+					<Typography
+						variant='h4'
+						style={{ margin: 18, fontFamily: 'Montserrat' }}
+					>
+						Cryptocurrency Prices by Market Cap
+					</Typography>
+				</Container>
+			</MuiThemeProvider>
+		</>
+	);
 };
 
 export default CoinsTable;
