e4c5c343cc71fde06dc4bcd036a0a43e58c85a0e
l33th <l33th@protonmail.ch>
2021-12-01 10:59:54 +0200

    alice carousal

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core';
 import { TrendingCoins } from '../../config/api';
 import axios from 'axios';
 import { CryptoState } from '../../CryptoContext';
+import AliceCarousel from 'react-alice-carousel';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
 	carousel: {
@@ -32,7 +34,43 @@ const Carousel = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [currency]);
 
-	return <div className={classes.carousel}>carousel</div>;
+  const items = trending.map((coin) => {
+    return (
+      <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+      </Link>
+      )
+  });
+
+  const responsive = {
+		0: {
+			items: 2,
+		},
+		512: {
+			items: 4,
+		},
+	};
+
+	return (
+		<div className={classes.carousel}>
+			<AliceCarousel
+				mouseTracking
+				infinite
+				autoPlayInterval={1000}
+				animationDuration={1500}
+				disableDotsControls
+				disableButtonsControls
+				responsive={responsive}
+				items={items}
+				autoPlay
+			/>
+		</div>
+	);
 };
 
 export default Carousel;
