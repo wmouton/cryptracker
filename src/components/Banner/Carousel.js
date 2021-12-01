b01dfd842cf3a1c5d2047ad0b7666d365056110e
l33th <l33th@protonmail.ch>
2021-12-01 10:15:31 +0200

    styles for carousel

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+	carousel: {
+		height: '50%',
+		display: 'flex',
+		alignItems: 'center',
+	},
+	carouselItem: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		cursor: 'pointer',
+		textTransform: 'uppercase',
+		color: 'white',
+	},
+}));
 
 const Carousel = () => {
-	return <div>carousel</div>;
+	const classes = useStyles();
+	return <div className={classes.carousel}>carousel</div>;
 };
 
 export default Carousel;
