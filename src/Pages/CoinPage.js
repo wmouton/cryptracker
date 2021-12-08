00cee8dd3fbeddc3c12f508fbcdc82b17a1a3033
l33th <l33th@protonmail.ch>
2021-12-08 06:42:28 +0200

    coin page with basic styles

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useParams } from 'react-router';
 import { SingleCoin } from '../config/api';
 import { CryptoState } from '../CryptoContext';
+import { makeStyles } from '@material-ui/core';
+import CoinInfo from '../components/CoinInfo';
 
 const CoinPage = () => {
 	const { id } = useParams();
@@ -23,11 +25,23 @@ const CoinPage = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	const useStyles = makeStyles((theme) => ({
+		container: {
+			display: 'flex',
+			[theme.breakpoints.down('md')]: {
+				flexDirection: 'column',
+				alignItems: 'center',
+			},
+		},
+	}));
 
+	const classes = useStyles();
 
 	return (
-		<div>
-			<h1>Coin Page</h1>
+		<div className={classes.container}>
+			<div className={classes.sidebar}>{/* sidebar */}</div>
+			{/* chart */}
+			<CoinInfo coin={coin} />
 		</div>
 	);
 };
---
+++ src/components/CoinInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinInfo = () => {
+	return (
+		<div>
+			<h1>Coin Info</h1>
+		</div>
+	);
+};
+
+export default CoinInfo;
