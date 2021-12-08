0d705a6aa77fedd1a6531844b33b37ec7f36b92b
l33th <l33th@protonmail.ch>
2021-12-08 06:23:38 +0200

    added coin data

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { SingleCoin } from '../config/api';
+import { CryptoState } from '../CryptoContext';
 
 const CoinPage = () => {
+	const { id } = useParams();
+	const [coin, setCoin] = useState();
+
+	const { currency, symbol } = CryptoState();
+
+	const fetchCoin = async () => {
+		const { data } = await axios.get(SingleCoin(id));
+
+		setCoin(data);
+	};
+
+	console.log(coin);
+
+	useEffect(() => {
+		fetchCoin();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+
+
 	return (
 		<div>
 			<h1>Coin Page</h1>
