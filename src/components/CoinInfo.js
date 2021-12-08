6081889c36ac1bc10f196a79bdfad204b9aef747
l33th <l33th@protonmail.ch>
2021-12-08 08:36:52 +0200

    coin info page setup code

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import { makeStyles, MuiThemeProvider } from '@material-ui/core';
+import { createTheme } from '@material-ui/core';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { HistoricalChart } from '../config/api';
+import { CryptoState } from '../CryptoContext';
 
-const CoinInfo = () => {
+const CoinInfo = ({ coin }) => {
+	const [historicData, setHistoricData] = useState();
+	const [days, setDays] = useState(1);
+	const { currency } = CryptoState();
+
+	const useStyles = makeStyles((theme) => ({}));
+
+	const classes = useStyles();
+
+	const fetchHistoricData = async () => {
+		const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+
+		setHistoricData(data.prices);
+	};
+
+	useEffect(() => {
+		fetchHistoricData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [days]);
+
+	const darkTheme = createTheme({
+		palette: {
+			primary: {
+				main: '#fff',
+			},
+			type: 'dark',
+		},
+	});
+
 	return (
-		<div>
-			<h1>Coin Info</h1>
-		</div>
+		<MuiThemeProvider theme={darkTheme}>
+			<div className={classes.container}></div>
+		</MuiThemeProvider>
 	);
 };
 
