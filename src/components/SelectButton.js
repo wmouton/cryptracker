38f88929a2874523ab7e64da1ab1e1a656ee922b
l33th <l33th@protonmail.ch>
2021-12-09 08:20:44 +0200

    full front-end cryptracer - final

--- src/components/SelectButton.js
+++ src/components/SelectButton.js
@@ -2,7 +2,26 @@ import { makeStyles } from '@material-ui/core';
 import React from 'react';
 
 const SelectButton = ({ children, selected, onClick }) => {
-	const useStyles = makeStyles({});
+	const useStyles = makeStyles({
+		selectbutton: {
+			border: '1px solid steelblue',
+			borderRadius: 5,
+			padding: 10,
+			paddingLeft: 20,
+			paddingRight: 20,
+			fontFamily: 'Montserrat',
+			cursor: 'pointer',
+			backgroundColor: selected ? 'steelblue' : '',
+			color: selected ? 'black' : '',
+			fontWeight: selected ? 700 : 500,
+			'&:hover': {
+				backgroundColor: 'steelblue',
+				color: 'black',
+			},
+			width: '22%',
+			//   margin: 5,
+		},
+	});
 
 	const classes = useStyles();
 
