dff8f987841f5aa97273361842e015c0e11420d4
l33th <l33th@protonmail.ch>
2021-12-26 08:18:05 +0200

    added MuiAlert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,6 +1,7 @@
 import { Snackbar } from '@material-ui/core';
 import React from 'react';
 import { CryptoState } from '../CryptoContext';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const Alert = () => {
   const { alert, setAlert } = CryptoState();
@@ -17,7 +18,16 @@ const Alert = () => {
       open={alert.open}
       autoHideDuration={3000}
       onClose={handleCloseAlert}
-    ></Snackbar>
+    >
+      <MuiAlert
+        onClose={handleCloseAlert}
+        elevation={10}
+        variant='filled'
+        severity={alert.type}
+      >
+        {alert.message}
+      </MuiAlert>
+    </Snackbar>
   );
 };
 
