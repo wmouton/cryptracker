42f673298f67759eb62c6879d1a002fd5a98d6a0
l33th <l33th@protonmail.ch>
2021-12-26 06:57:20 +0200

    auth and database variables and imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import firebaseConfig from './config/firebaseConfig';
 
 const firebaseApp = initializeApp(firebaseConfig);
+
+const aut = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
