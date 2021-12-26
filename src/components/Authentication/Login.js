060ea84f9d930cbdb23f6044314c7074382b7026
l33th <l33th@protonmail.ch>
2021-12-26 07:47:58 +0200

    added box to signup component

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Login = () => {
+const Login = ({ handleClose }) => {
     return (
         <div>
             Login
--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import { Box } from '@material-ui/core';
+import React, { useState } from 'react';
 
-const Signup = () => {
-    return (
-        <div>
-            Signup
-        </div>
-    )
-}
+const Signup = ({ handleClose }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-export default Signup
+  return (
+    <Box
+      p={3}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '20px',
+      }}
+    ></Box>
+  );
+};
+
+export default Signup;
