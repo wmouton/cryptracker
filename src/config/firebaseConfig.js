f8f2c8c5a5fff589db5c3936933789f300052e93
l33th <l33th@protonmail.ch>
2021-12-26 06:31:23 +0200

    firebase config file setup

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAqIdIbCu_NBHPtA4PwOYdIm2cKbBiI9ag',
+  authDomain: 'cryptracer-crypto-tracker.firebaseapp.com',
+  projectId: 'cryptracer-crypto-tracker',
+  storageBucket: 'cryptracer-crypto-tracker.appspot.com',
+  messagingSenderId: '1005124842956',
+  appId: '1:1005124842956:web:dfa068a23af8f6e93e5969',
+};
+
+export default firebaseConfig;
