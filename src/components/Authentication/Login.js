07cee91bf3100f8d9cf3e2848292c03d1e18fa3f
l33th <l33th@protonmail.ch>
2021-12-26 08:02:46 +0200

    added text fields and login button to Login.js

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import { Box, Button, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
 
 const Login = ({ handleClose }) => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleSubmit = () => {};
+
+  return (
+    <Box
+      p={3}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '20px',
+      }}
+    >
+      <TextField
+        variant='outlined'
+        type='email'
+        label='Enter Email'
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        fullWidth
+      />
+      <TextField
+        variant='outlined'
+        label='Enter Password'
+        type='password'
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        fullWidth
+      />
+      <Button
+        variant='contained'
+        size='large'
+        onClick={handleSubmit}
+        style={{ backgroundColor: 'steelblue' }}
+      >
+        Login
+      </Button>
+    </Box>
+  );
+};
+
+export default Login;
