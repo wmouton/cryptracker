57ea523860e482cdcddf20e5aa19fc1500e77315
l33th <l33th@protonmail.ch>
2021-12-26 08:15:40 +0200

    added handleClose function to Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,17 +1,24 @@
 import { Snackbar } from '@material-ui/core';
-import React from 'react'
+import React from 'react';
 import { CryptoState } from '../CryptoContext';
 
 const Alert = () => {
-    const { alert, setAlert } = CryptoState();
+  const { alert, setAlert } = CryptoState();
 
-    return (
-        <Snackbar
+  const handleCloseAlert = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setAlert({ open: false });
+  };
+
+  return (
+    <Snackbar
       open={alert.open}
       autoHideDuration={3000}
       onClose={handleCloseAlert}
     ></Snackbar>
-    )
-}
+  );
+};
 
-export default Alert
+export default Alert;
