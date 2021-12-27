dae1d465aa0a59e093b26b73f448c802f466bcf8
l33th <l33th@protonmail.ch>
2021-12-27 06:58:14 +0200

    handleSubmit validation 1

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,11 +1,23 @@
 import { Box, Button, TextField } from '@material-ui/core';
 import React, { useState } from 'react';
+import { CryptoState } from '../../CryptoContext';
 
 const Login = ({ handleClose }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = () => {};
+  const { setAlert } = CryptoState();
+
+  const handleSubmit = () => {
+    if (!email || !password) {
+      setAlert({
+        open: true,
+        message: "Please fill in all the fields.",
+        type: "error",
+      });
+      return;
+    }
+  };
 
   return (
     <Box
