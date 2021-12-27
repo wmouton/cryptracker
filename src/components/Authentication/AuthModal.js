9d1e40185f6f879b3628066a001a8443106342d8
l33th <l33th@protonmail.ch>
2021-12-27 08:04:05 +0200

    google button import

--- src/components/Authentication/AuthModal.js
+++ src/components/Authentication/AuthModal.js
@@ -3,9 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-import { AppBar, Button, Tab, Tabs } from '@material-ui/core';
+import { AppBar, Box, Button, Tab, Tabs } from '@material-ui/core';
 import Login from './Login';
 import Signup from './Signup';
+import GoogleButton from 'react-google-button';
 
 const useStyles = makeStyles(theme => ({
   modal: {
