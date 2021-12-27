77215053ceb96dc081f4477324da1609f2f74c66
l33th <l33th@protonmail.ch>
2021-12-27 07:18:44 +0200

    added user sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AuthModal from './Authentication/AuthModal';
+import UserSidebar from './Authentication/UserSidebar';
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 
