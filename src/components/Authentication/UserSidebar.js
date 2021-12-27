c8bba8a2543f8b4c5742984db013795c5d7b0382
l33th <l33th@protonmail.ch>
2021-12-27 07:25:17 +0200

    added sidebar drawer

--- src/components/Authentication/UserSidebar.js
+++ src/components/Authentication/UserSidebar.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
-const UserSidebar = () => {
-  return <div>UserSidebar</div>;
-};
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
 
-export default UserSidebar;
+export default function UserSidebar() {
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+
+  return (
+    <div>
+      {['right'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+          Something
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
