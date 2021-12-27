bbf2019534ca296c566a0ec558215204951d0089
l33th <l33th@protonmail.ch>
2021-12-27 07:34:08 +0200

    user sidebar style

--- src/components/Authentication/UserSidebar.js
+++ src/components/Authentication/UserSidebar.js
@@ -2,41 +2,59 @@ import React from 'react';
 
 import Drawer from '@material-ui/core/Drawer';
 import { CryptoState } from '../../CryptoContext';
-import { Avatar } from '@material-ui/core';
+import { Avatar, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    width: 350,
+    padding: 25,
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    fontFamily: 'monospace',
+  },
+});
 
 export default function UserSidebar() {
+  const classes = useStyles();
   const [state, setState] = React.useState({
     right: false,
   });
 
   const { user } = CryptoState();
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+  const toggleDrawer = (anchor, open) => event => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return;
     }
 
     setState({ ...state, [anchor]: open });
   };
 
-
   return (
     <div>
-      {['right'].map((anchor) => (
+      {['right'].map(anchor => (
         <React.Fragment key={anchor}>
           <Avatar
             onClick={toggleDrawer(anchor, true)}
             style={{
               height: 38,
               width: 38,
-              cursor: "pointer",
-              backgroundColor: "steelblue",
+              cursor: 'pointer',
+              backgroundColor: 'steelblue',
             }}
             src={user.photoURL}
             alt={user.displayName || user.email}
           />
-          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-          Something
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            <div className={classes.container}></div>
           </Drawer>
         </React.Fragment>
       ))}
